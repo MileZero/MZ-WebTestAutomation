f83a0a8b7049b5944904f1184811d687bcb47510
Mohamed Afifi <mohamedafifiabdellatif@gmail.com>
2023-06-22 18:24:18 +0300

    adding TCs

--- tests/first.js
+++ tests/first.js
@@ -11,7 +11,7 @@ const browser = await chromium.launch({headless:false, slowMo:100});
 
 const page = await browser.newPage();
 
-await page.goto('https://www.google.com');
+await page.goto('https://milevision-stage.milezero.com');
 
 await browser.close();
 
