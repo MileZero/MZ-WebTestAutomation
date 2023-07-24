445e980f343aa90509c21f44606ccf83b73e3099
Yasser el shrief <yhesham@milezero.com>
2023-07-24 17:43:13 +0300

    login added

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,13 +1,24 @@
 // @ts-check
-const { test, expect} = require('@playwright/test');
-const { setup }  = require()
-const userName = process.env.USER || '';
-const password = process.env.PASSCODE || '';
-
-test('test', async ({ page }) => {
+const { test, expect} = require ('@playwright/test');
+async function login(page, email, password) {
+  
   await page.goto('https://milevision-stage.milezero.com/');
-  await page.getByPlaceholder('username/email').fill(userName);
+
+  // Fill in email and password and click login button
+
+  await page.getByPlaceholder('username/email').fill(email)
   await page.getByPlaceholder('your password').fill(password);
   await page.getByRole('button', { name: 'Log In' }).click();
-  await page.getByTitle('Configure').click();
-});
+
+  // Wait for navigation to complete and check if login was successful
+
+  await page.waitForNavigation();
+  const title = await page.title();
+  if (title === 'Milevision') {
+    console.log('Login successful');
+  } else {
+    console.log('Login failed');
+  }
+}
+
+module.exports = login;
